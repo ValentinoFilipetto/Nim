a7a9f203eba47ef0697c0067ebfcf5e6264d05ef
ValentinoFilipetto <valefilipetto@gmail.com>
2024-07-20 23:44:30 +0200

    chore: remove ununsed import

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.HashMap;
 
 public class Main {
 
@@ -35,7 +34,6 @@ public class Main {
             } else {
                 game.myMap = game.decreasePile(game.myMap, pile, amount);
             }
-
 
             if (currentPlayer.equals(namePlayer1)) {
                 currentPlayer = namePlayer2;
